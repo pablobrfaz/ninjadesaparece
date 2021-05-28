b698476bf7ece16eb2f03a5561b106a1959c3e51
pablobrfaz <pablojbravofaz@gmail.com>
2021-05-28 15:24:17 -0500

    Ninjas desaparecen

---
+++ ninja.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {      /* Evento que sale un mensaje cuando se aplasta un boton */
+    $("#ninja1").click(function(){
+        
+        $("#ninja1").hide(3000); 
+        
+    });
+    $("#ninja2").click(function(){
+        
+        $('#ninja2').hide(5000); 
+        
+    });
+    $("#ninja3").click(function(){
+        
+        $('#ninja3').hide(3000); 
+        
+    });
+    $("#ninja4").click(function(){
+        
+        $('#ninja4').hide(3000); 
+        
+    });
+    $("#ninja5").click(function(){
+        
+        $("#ninja5").hide(3000); 
+        
+    });
+    $("#ninja6").click(function(){
+        
+        $('#ninja6').hide(5000); 
+        
+    });
+    $("#ninja7").click(function(){
+        
+        $('#ninja7').hide(3000); 
+        
+    });
+    $("#ninja8").click(function(){
+        
+        $('#ninja8').hide(3000); 
+        
+    });
+
+    $(".boton1").click(function(){
+        
+        $('img').show(3000); 
+        
+    });
+});
